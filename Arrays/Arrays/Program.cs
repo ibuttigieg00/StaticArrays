d5da66d00d48c7d005daf13c148857492f2b9e9e
izaak buttigieg <izaakbuttigieg00@gmail.com>
2023-07-10 14:22:44 +0200

    Added multiple examples

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using Arrays;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+        int val = 2;
+
+        int [] orderedNums = solution.OrderList(nums);
+        Console.WriteLine(solution.ReadAllElements(orderedNums));
+
+        orderedNums = solution.RemoveLastElement(orderedNums);
+        Console.WriteLine(solution.ReadAllElements(orderedNums));
+
+        //Console.WriteLine(solution.RemoveElement(nums, val));
+
+        Console.ReadLine();
+    }
+}
---
+++ Arrays/Arrays/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Solution
+    {
+        // Reads all of the elements in the array
+        public string ReadAllElements(int[]nums)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                sb.Append(nums[i]);
+            }
+            return sb.ToString();
+        }
+        // Remove the last index of the array
+        public int[] RemoveLastElement(int[]nums)
+        {
+            nums[nums.Length - 1] = 0;
+            return nums;
+        }
+
+        // Make an un-ordered list ordered
+        public int[] OrderList(int[]nums)
+        {
+            int replaced = 0;
+            bool replacementsMade = true;
+
+            while (replacementsMade)
+            {
+                replacementsMade = false;
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (nums[i] < nums[i - 1])
+                    {
+                        replaced = nums[i];
+                        nums[i] = nums[i - 1];
+                        nums[i - 1] = replaced;
+                        replacementsMade = true;
+                    }
+                }
+            }           
+            return nums;
+        }
+        
+        public int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] != val)
+                {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+            return k;
+        }
+    }
+}
